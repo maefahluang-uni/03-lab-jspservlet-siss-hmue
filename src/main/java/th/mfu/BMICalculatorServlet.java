afb448ef4f456e8b6f5f81712e7be6ea6f014659
siss hmue <6531503184@lamduan.mfu.ac.th>
2023-09-05 18:59:01 +0700

    finish

--- src/main/java/th/mfu/BMICalculatorServlet.java
+++ src/main/java/th/mfu/BMICalculatorServlet.java
@@ -9,15 +9,22 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 //TODO: add webservlet to "/calbmi"
+@WebServlet("/calbmi")
 public class BMICalculatorServlet extends HttpServlet{
 
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         //TODO: get parameter from request: "weight" and "height"
-        
+        String weightinStr = request.getParameter("weight");
+        String heightinStr = request.getParameter("height");
+        response.setContentType("text/html");
         //TODO: calculate bmi
-
+        double weight = Double.parseDouble(weightinStr);
+        double height = Double.parseDouble(heightinStr);
+        double result = weight/(height*height);
+        result = Math.round(result);
         //TODO: determine the built from BMI
+        
       
         //TODO: add bmi and built to the request's attribute
 
